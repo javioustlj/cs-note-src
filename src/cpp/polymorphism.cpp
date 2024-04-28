47756cda0c638b0ec07f1d4b079bd1473384d81e
Tian Javious <javioustlj@outlook.com>
2024-04-28 12:31:13 +0300

    update polymorphism

--- src/cpp/polymorphism.cpp
+++ src/cpp/polymorphism.cpp
@@ -37,6 +37,34 @@ private:
     int b;
 };
 
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base::Base()" << endl;
+    };
+    ~Base() { cout << "Base::~Base()" << endl; };
+    virtual void virtual_fun1() { cout <<"Base::virtual_fun1()" << endl; };
+    void func2() { cout <<"Base::func2()" << endl; };
+};
+
+class Derived : public Base
+{
+public:
+    Derived() : a(0), b(0)
+    {
+        cout << "Derived::Derived()" << endl;
+    }
+    ~Derived() { cout << "Derived::~Derived()" << endl; };
+    virtual void virtual_fun1() { cout <<"Derived::virtual_fun1()" << endl; };
+    void func2() { cout <<"Derived::func2()" << endl; };
+private:
+    int a;
+    int b;
+};
+
+
 /* 在存在虚函数的类中，虚函数指针占正常指针大小的字节
  * this指针不占用大小
  */
@@ -52,8 +80,21 @@ void test_sizeof_virtual_pointer()
     delete p;
 }
 
+
+void test_virtual_destructor()
+{
+    cout << "==================start test_virtual_destructor=============" << endl;
+    Base *p1 = new Derived;
+    delete p1;
+    Derived *p2 = new Derived;
+    delete p2;
+    cout << "==================end test_virtual_destructor=============" << endl;
+}
+
+
 int main()
 {
     test_sizeof_virtual_pointer();
+    test_virtual_destructor();
     return 0;
 }
